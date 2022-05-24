5f20b6e80cf0319a60d14521fb8e46d796de96cf
Azeem Mohammed Basheer <azeem@keyvalue.systems>
2022-05-24 11:12:36 +0530

    Small changes

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,14 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/azeem/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/azeem/Projects/Bloc vs Getx/getx_demo" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/azeem/flutter"
+  "PROJECT_DIR=/home/azeem/Projects/Bloc vs Getx/getx_demo"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=/home/azeem/Projects/Bloc vs Getx/getx_demo/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=lib/main.dart"
+)
